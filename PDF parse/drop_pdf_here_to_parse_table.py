5e9d9aa406f2339121d478e4de4f793a628cb40c
akirbaes <akirbaes@ulb.ac.be>
2021-03-03 12:19:35 +0100

    Simple script using tabula to isolate tables from pdfs

---
+++ PDF parse/drop_pdf_here_to_parse_table.py
@@ -0,0 +1,11 @@
+import tabula
+import traceback
+import sys
+for filename in sys.argv[1:]: 
+    print(filename)
+    print(filename[:-4]+".csv")
+    try:
+        tabula.convert_into(filename, filename[:-4]+".csv", output_format="csv", pages='all')
+    except Exception as e:
+        traceback.print_exc()
+input("Press ANY key...")
