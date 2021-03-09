37d6e83ff3cd655f974cee2f1259aafb99b16e7c
akirbaes <akirbaes@ulb.ac.be>
2021-03-09 16:15:13 +0100

    Try fix merging columns bug

--- PDF parse/drop_pdf_here_to_parse_table.py
+++ PDF parse/drop_pdf_here_to_parse_table.py
@@ -5,7 +5,7 @@ for filename in sys.argv[1:]:
     print(filename)
     print(filename[:-4]+".csv")
     try:
-        tabula.convert_into(filename, filename[:-4]+".csv", output_format="csv", pages='all')
+        tabula.convert_into(filename, filename[:-4]+".csv", output_format="csv", pages='all', stream=True, guess=True, lattice=True)
     except Exception as e:
         traceback.print_exc()
 input("Press ANY key...")
